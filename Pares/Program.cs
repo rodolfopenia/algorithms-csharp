a4a15cffea4a4bfb580eb7527f0fed40929dbda6
Rodolfo Peña <rodolfo.penia@hotmail.com>
2023-04-10 02:22:31 -0500

    Algoritmos resueltos de números primos y pares

---
+++ Pares/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+int startNumber, endNumber;
+Console.WriteLine("Ingrese el rango inicial: ");
+int.TryParse(Console.ReadLine(), out startNumber);
+Console.WriteLine("Ingrese el rango final: ");
+int.TryParse(Console.ReadLine(), out endNumber);
+
+for (int i = startNumber; i <= endNumber; i++) {
+  if (i % 2 == 0) {
+    Console.WriteLine($"El número {i} es par.");
+  } else {
+    Console.WriteLine($"El número {i} es impar.");
+  }
+}
+
+
---
+++ Primos/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+int startNumber, endNumber;
+int count = 0;
+Console.WriteLine("Ingrese un rango inicial: ");
+int.TryParse(Console.ReadLine(), out startNumber);
+Console.WriteLine("Ingrese un rango final: ");
+int.TryParse(Console.ReadLine(), out endNumber);
+
+for(int i = startNumber; i <= endNumber; i++) {
+  count = 0;
+  for(int j = 1; j <= i; j++){
+    if (i % j == 0) {
+      count++;
+    }
+  }
+
+  if (count == 2) {
+    Console.WriteLine($"El número {i} es primo.");
+  } else {
+    Console.WriteLine($"El número {i} no es primo.");
+  }
+}
+
+
+
