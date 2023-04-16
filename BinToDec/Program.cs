a180bb6da7b3cc48d4866df970131e64190a16bc
Rodolfo Peña <rodolfo.penia@hotmail.com>
2023-04-16 02:08:22 -0500

    Ejercicio para convertir un numero binario a decimal

---
+++ BinToDec/Program.cs
@@ -0,0 +1,16 @@
+﻿string? binaryNumber;
+string digit;
+int product;
+int pow = 0;
+int number = 0;
+Console.WriteLine("Ingrese un número binario: ");
+binaryNumber = Console.ReadLine();
+
+for(int i = binaryNumber!.Length - 1; i >= 0; i--) {
+  digit = binaryNumber[i].ToString();
+  product = int.Parse(digit) * (int) Math.Pow(2,pow);
+  number = number + product;
+  pow++;
+}
+
+Console.WriteLine($"El número convertido a sistema decimal es: {number}");
