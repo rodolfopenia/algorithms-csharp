c6081132aca4b2d965d9d9d1de012c1ebbea187a
Rodolfo Peña <rodolfo.penia@hotmail.com>
2023-04-16 01:20:21 -0500

    Ejercicio para convertir un numero a binario

---
+++ DecToBin/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+int number;
+int residue;
+string binaryNumber = "";
+Console.WriteLine("Ingrese un número: ");
+int.TryParse(Console.ReadLine(), out number);
+
+while(number >= 2) {
+  residue = number % 2;
+  number = number / 2;
+
+  if(number == 1) {
+    binaryNumber = number.ToString() + residue.ToString() + binaryNumber;
+  } else {
+    binaryNumber = residue.ToString() + binaryNumber;
+  }
+}
+
+Console.WriteLine($"El número binario es: {binaryNumber}");
