e8286d04b45d1debdc7fead2e29e9b27e1788b62
Rodolfo Peña <rodolfo.penia@hotmail.com>
2023-09-20 23:44:26 -0500

    Roman Number example

---
+++ romanNumberToDec/Program.cs
@@ -0,0 +1,22 @@
+﻿int [] arrRomanValues = {1000, 500, 100, 50, 10, 5, 1};
+string romanLetters = "MDCLXVI";
+string? romanNumber;
+int position;
+int number = 0;
+int lastValue = 0;
+
+Console.WriteLine("Ingrese un número romano: ");
+romanNumber = Console.ReadLine();
+
+for(int i = 0; i < romanNumber!.Length; i++) {
+  position = romanLetters.IndexOf(romanNumber[i]);
+  number = number + arrRomanValues[position];
+
+  if (arrRomanValues[position] > lastValue) {
+    number = number - (2 * lastValue);
+  }
+
+  lastValue = arrRomanValues[position];
+}
+
+Console.WriteLine($"El número romano en decimal es: {number}");
---
+++ romanNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿string [] arrUnits = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+string [] arrTens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+string [] arrHundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+string [] arrThousands = {"", "M", "MM", "MMM"};
+int number;
+int units;
+int tens;
+int hundreds;
+int thousands;
+string romanNumber = "";
+Console.WriteLine("Ingrese un número: ");
+int.TryParse(Console.ReadLine(), out number);
+units = number % 10;
+number = number / 10;
+tens = number % 10;
+number = number / 10;
+hundreds = number % 10;
+number = number / 10;
+thousands = number % 10;
+romanNumber = $"{arrThousands[thousands]}";
+romanNumber = $"{romanNumber}{arrHundreds[hundreds]}";
+romanNumber = $"{romanNumber}{arrTens[tens]}";
+romanNumber = $"{romanNumber}{arrUnits[units]}";
+Console.WriteLine($"El número en romano es: {romanNumber}");
+
+
+
