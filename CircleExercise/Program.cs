f5dcfbf6c08df6800f327023cf942e7a4d187773
Rodolfo Peña <rodolfo.penia@hotmail.com>
2023-11-12 00:32:16 -0500

    Yellow Flowers and Circle Excercise

---
+++ CircleExercise/Program.cs
@@ -0,0 +1,15 @@
+﻿double radio;
+double area, girth;
+
+Console.WriteLine("Ingrese un radio del círculo:");
+radio = double.Parse(Console.ReadLine());
+
+girth = 2 * radio * Math.PI;
+
+area = Math.PI * Math.Pow(radio, 2);
+
+girth = Math.Round(girth, 2);
+area = Math.Round(area, 2);
+
+Console.WriteLine($"La circunferencia del círculo es: {girth}");
+Console.WriteLine($"El área del círculo es: {area}");
---
+++ yellowFlowers/Program.cs
@@ -0,0 +1,75 @@
+﻿/*
+
+*     * * *
+* *   * *
+* * * *
+    * * * *
+  * *   * *
+* * *     *
+   *
+  *
+  *
+   *
+     *
+*/
+
+int number = 5;
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("*********************");
+Thread.Sleep(200);
+Console.WriteLine("**FOR YU MAI LOF!!!!**");
+Thread.Sleep(200);
+Console.WriteLine("*********************");
+Thread.Sleep(200);
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+
+for(int i = 0; i < number; i++){
+  for(int j = 0; j < i + 1; j++){
+    Console.Write(" *");
+  }
+
+  for(int j = number; j > i + 1; j--){
+    Console.Write("  ");
+  }
+
+  for(int j = number; j > i; j--){
+    Console.Write(" *");
+  }
+  Console.WriteLine();
+  Thread.Sleep(200);
+}
+
+for(int i = 0; i < number; i++){
+  for(int j = number; j > i + 1; j--){
+    Console.Write("  ");
+  }
+
+  for(int j = 0; j < i + 1; j++){
+    Console.Write(" *");
+  }
+
+  for(int j = 0; j < i; j++){
+    Console.Write("  ");
+  }
+
+  for(int j = number; j > i; j--){
+    Console.Write(" *");
+  }
+  Console.WriteLine();
+  Thread.Sleep(200);
+}
+
+// stem
+Console.ForegroundColor = ConsoleColor.Green;
+for(int i = 0; i < 6; i++){
+  for(int j = 0; j < number - 2; j++){
+    Console.Write("  ");
+  }
+  Console.Write(" *");
+  if(i <= 0) number -= 1;
+  if(i >= 3) number += 1;
+  Console.WriteLine();
+  Thread.Sleep(200);
+}
